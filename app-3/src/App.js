2c7687a2dbb912f8f53a348dbae73c836f20b59d
Maikalanimai <kalonekamimoto@gmail.com>
2019-12-06 15:33:34 -0700

    apps 1-3 complete

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -3,16 +3,28 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      list: ['coke','sprite', 'monster', 'dr. pepper'],
+      input: ''
+    }
+  }
+
+  handleChange(e){
+    this.setState({
+      input: e.target.value
+    })
+  }
+
   render() {
+  let listDisplay = this.state.list.filter(element => element.includes(this.state.input))
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <input onChange = {e => this.handleChange(e)}/>
+        {listDisplay.map((element)=> {
+          return <h2>{element}</h2>
+        })}
       </div>
     );
   }
